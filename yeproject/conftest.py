467242dee2e5c969e0398faf9e84a7bdeece6556
shangkyu.shin <sskyu2@naver.com>
2022-09-16 18:37:09 +0900

    yeproject_first

---
+++ yeproject/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from yeproject.users.models import User
+from yeproject.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
